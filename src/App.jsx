e2525a095deaf5f7526a7b19c0235099f68baa39
gbmsaraujo <gbmsaraujo@gmail.com>
2022-05-10 16:11:10 -0300

    Criação dos componentes discussões e posts

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,11 @@
 import "./App.scss";
 
-
 import Aside from "./components/Aside";
+import Discussoes from "./components/Discussoes";
 import Header from "./components/Header";
+import Posts from "./components/Posts";
 import Resumo from "./components/Resumo";
 import SectionMain from "./components/Section-Main";
-
 
 function App() {
 	return (
@@ -15,7 +15,14 @@ function App() {
 
 			<main className="ct-main">
 				<SectionMain/>
-				<Resumo />	
+				<Resumo />
+				<Discussoes/>
+				<Posts 
+				classe="ct-posts"
+				like="1 like" 
+				resposta="1 resposta"/>
+				<Posts
+				classe="ct-posts-select" />
 			</main>
 		</div>
 	);
--- src/components/Discussoes/index.jsx
+++ src/components/Discussoes/index.jsx
@@ -1,7 +1,34 @@
+import './styles.scss'
+import icon1 from './icons/icone1.svg' 
+import icon2 from './icons/icone2.svg' 
+import icon3 from './icons/icone3.svg' 
+import { AiOutlinePlus } from "react-icons/ai"
+
 const Discussoes = () => {
     return(
         <section className="ct-discussoes">
 
+            <div className="box-title">
+                <h2> Discussões </h2>
+            </div>
+
+            <h2>Compartilhe suas ideias ou dúvidas com os autores!</h2>
+
+            <div className='ct-discussoes__icons'>
+                <img src={icon1} alt="Ícone 1" />
+                <img src={icon2} alt="Ícone 2" />
+                <img src={icon3} alt="Ícone 2" />
+            </div>
+
+        <p>Sabia que o maior estímulo no desenvolvimento científico e cultural é a curiosidade? 
+            Deixe seus questionamentos ou sugestões para o autor!</p>
+
+            <div className="ct-btns">
+                <button> <AiOutlinePlus className='ct-btns__icon' /> Criar Tópico </button>
+            </div>
+
         </section>
     )
 }
+
+export default Discussoes
